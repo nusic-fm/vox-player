221863d9bb7d95ede43acdb3b4286a67c0326cb9
logesh2496 <logesh.r24@gmail.com>
2024-06-10 20:20:08 +0530

    fix: without avatars

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -16,6 +16,7 @@ export const getYouTubeVideoId = (url: string) => {
 };
 
 export const getUserAvatar = (uid: string, avatarId: string) => {
+  if (!avatarId) return "";
   if (avatarId.length <= 2) {
     return `https://firebasestorage.googleapis.com/v0/b/nusic-vox-player.appspot.com/o/avatars%2F${avatarId}.webp?alt=media`;
   }
